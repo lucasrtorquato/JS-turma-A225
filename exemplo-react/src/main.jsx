35bae36b1fe489a534997290f48cc886d05a0e77
Lucas Torquato <lucas.torquato.senai@gmail.com>
2025-10-25 12:01:58 -0300

    react

---
+++ exemplo-react/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Titulo from './Titulo.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Titulo />
+    <Titulo titulo='Outro Título'/>
+    <App />
+  </StrictMode>,
+)
