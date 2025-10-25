f548aa50cb29cc432f5436bd818de1d3be1759e5
Lucas Torquato <lucas.torquato.senai@gmail.com>
2025-10-25 09:34:40 -0300

    finalização cadastro

--- exercicios-individuais/js/projetos/cadastro/cadastro.js
+++ exercicios-individuais/js/projetos/cadastro/cadastro.js
@@ -31,10 +31,18 @@ const tabelaCorpo = document.querySelector("#user-table-body");
 let idEmEdicao = null;
 
 const btnCep = document.querySelector("#btn-buscar-cep");
-
-
 const inputBusca = document.querySelector("#user-busca");
 
+//modal
+const modalDetalhes = document.querySelector("#detalhes-modal");
+const modalNome = document.querySelector("#modal-nome");
+const modalEmail = document.querySelector("#modal-email");
+const modalEndereco = document.querySelector("#modal-endereco-completo");
+const modalObs = document.querySelector("#modal-obs");
+const modalBtnEditar = document.querySelector("#modal-btn-editar");
+const modalBtnExcluir = document.querySelector("#modal-btn-excluir");
+
+const modal = new bootstrap.Modal(modalDetalhes);
 
 
 
@@ -100,6 +108,8 @@ function renderizarTabela(usuariosFiltrados = usuarios){
                 <button type="button" class="btn btn-sm btn-warning" data-id="${user.id}">Editar</button>
 
                 <button type="button" class="btn btn-sm btn-danger" data-id="${user.id}">Excluir</button>
+
+                <button type="button" class="btn btn-sm btn-primary" data-id="${user.id}">Mostrar Detalhes</button>
             </td>
         `;
 
@@ -202,8 +212,54 @@ function downloadArquivo(){
     URL.revokeObjectURL(url);
 }
 
-function uploadArquivo(){
+function uploadArquivo(event){
+    const arquivo = event.target.files[0];
 
+    if (!arquivo) return;
+
+    const leitor = new FileReader();
+
+    leitor.onload = function(e){
+        //pega o resultado da leitura do arquivo
+        const conteudoArquivo = e.target.result;
+
+        const usuariosImportados = JSON.parse(conteudoArquivo);
+
+        if (!Array.isArray(usuariosImportados)){
+            alert("Arquivo não é um array valido!");
+            return;
+        }
+
+        if (confirm("Deseja realmente substituir as informações dos usuários?")){
+            usuarios = usuariosImportados;
+            salvarNoStorage();
+            renderizarTabela();
+            alert("Usuários importados com sucesso!");
+            inputUpload.value = "";
+            //location.reload(true);
+        }
+    }
+    leitor.readAsText(arquivo);
+    
+}
+
+function mostrarDetalhesUsuario(id){
+    const user = usuarios.find(u => u.id === id);
+
+    if (!user) return;
+
+    modalNome.textContent = `${user.nome} ${user.sobrenome}`;
+    modalEmail.textContent = user.email;
+
+    const endereco = [user.rua, user.numero, user.complemento, user.bairro, user.cidade, user.estado, user.cep].filter(Boolean).join(", ");
+
+    modalEndereco.textContent = endereco;
+    modalObs.textContent = user.obs;
+
+    modalBtnEditar.dataset.id = user.id;
+    modalBtnExcluir.dataset.id = user.id;
+
+    modal.show();
 }
 
 function inicializar(){
@@ -232,7 +288,26 @@ function inicializar(){
             editarUsuario(id);
         } else if (target.classList.contains("btn-danger")){
             excluirUsuario(id);
-        }        
+        } else if (target.classList.contains("btn-primary")){
+            mostrarDetalhesUsuario(id);
+        }
+    })
+
+    modalDetalhes.addEventListener("click", (event) => {
+        const target = event.target.closest("button");
+        if(!target) return
+        
+        const id = Number(target.dataset.id);
+        
+        if (isNaN(id)) return
+
+        if (target.classList.contains("btn-warning")){
+            modal.hide();
+            editarUsuario(id);
+        } else if (target.classList.contains("btn-danger")){
+            modal.hide();
+            excluirUsuario(id);
+        }
     })
 }
 
